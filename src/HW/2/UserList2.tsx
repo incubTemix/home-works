6afea0f7981018635263bf1e8e0d7308ce51abb4
incubTemix <142362752+incubTemix@users.noreply.github.com>
2023-09-21 18:11:46 +0300

    commit

---
+++ src/HW/2/UserList2.tsx
@@ -0,0 +1,23 @@
+import {CurrentUser} from './CurrentUser';
+import {UsersObjectType, UserType} from './HW2';
+
+type UserList2PropsType = {
+	users: UsersObjectType
+	filterUsers:(city: string) => void
+};
+
+export const UserList2 = (props: UserList2PropsType) => {
+	return (
+		<div id={'hw02-users'}>
+			<h2>User List:</h2>
+
+			<button id={'hw02-filter-button'} onClick={() => props.filterUsers('Los Angeles')}>SHOW ME FRIENDS FROM LA</button>
+
+			<ul>
+			  {props.users.myFriends.map((user) => (
+			    <CurrentUser key={user.id} user={user} />
+			  ))}
+			</ul>
+		</div>
+	);
+};
